7831496709793ada57df54a1d70030c78b3b4f3a
I-Sicarius-I <game7profile@gmail.com>
2026-09-14 12:50:35 +0200

    Created automated tests for add to cart

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import formatCurrency from "../scripts/utils/money.js";
+import formatCurrency from "../../scripts/utils/money.js";
 
 describe('test suite: formatCurrency', () => {
     it('converts cents into dollars', () => {
@@ -7,13 +7,13 @@ describe('test suite: formatCurrency', () => {
 
     it('works with 0', () => {
         expect(formatCurrency(0)).toEqual('0.00')
-    })
+    });
 
     it('rounds up to nearest cent', ()=>{
         expect(formatCurrency(2000.5)).toEqual('20.01')
-    })
+    });
 
     it('rounds down to nearest cent', () => {
         expect(formatCurrency(2000.4)).toEqual('20.00')
-    })
+    });
 });
